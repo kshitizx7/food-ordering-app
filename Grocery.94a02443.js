de4dd64b7c2b2ad7f15d07c218b0f5505ab260ce
kshitiz sharma <kshitizsha17@gmail.com>
2025-08-31 05:10:01 +0530

    Updates

---
+++ Grocery.94a02443.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire5906;(0,e.register)("loR3J",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0"),t=()=>(0,i.jsx)("div",{children:"this is a grocery component"})});
+//# sourceMappingURL=Grocery.94a02443.js.map
