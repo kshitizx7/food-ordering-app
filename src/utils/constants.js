e34f096f135d679b6834ccd3c6399ef5c04bff02
kshitiz sharma <kshitizsha17@gmail.com>
2025-08-30 19:24:04 +0530

    added a new logo by uploading it online on a website

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,8 @@
-export const LOGO_URL = "https://cdn.dribbble.com/userupload/16778067/file/original-d75cb39663149843b1572e4cc64681fe.jpg?resize=400x0" ;
+export const LOGO_URL = "https://i.ibb.co/GQyL43LP/Gemini-Generated-Image-8nngvp8nngvp8nng.png";
 
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.8466937&lng=80.94616599999999&restaurantId="
-
 
 export const USER_URL = "https://api.github.com/users/";
 
